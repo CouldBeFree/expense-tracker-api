2a5af3a1b39a0cb43fb841b2854639aafdbb3ae3
couldbefree <zoriaalexandr@gmail.com>
2023-01-10 20:49:33 +0200

    connected to mongo

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
 	_ "fmt"
+	"log"
 
 	handlers "expense-tracker-api/handlers"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+var ctx context.Context
+var err error
+var client *mongo.Client
+var collection *mongo.Collection
 
 var authHandler *handlers.AuthHandler
 
 func main() {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
+
 	router := gin.Default()
 	router.POST("/register", authHandler.RegisterUser)
 	router.Run(":5050")
